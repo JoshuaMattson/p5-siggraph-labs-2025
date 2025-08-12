5c7cc80d7d595b836266dc48553078cd6518732f
Joshua Mattson <JoshuaMattson3@gmail.com>
2025-08-12 12:57:44 -0700

    Update data.js
    My postcard!

--- data.js
+++ data.js
@@ -1,3 +1,4 @@
 const postcards = [
   { sketch: 'https://editor.p5js.org/davepagurek/sketches/7YRHVfZRl', canUseInMedia: true },
+    sketch: 'https://editor.p5js.org/j-weezy/sketches/Uf-OmYAm0', canUseInMedia: true },
 ];
